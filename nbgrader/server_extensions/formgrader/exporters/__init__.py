349aab4338dda52a81dd0657c49b6bbb355cf275
Tim Metzler <tim.metzler@inf.h-brs.de>
2020-02-06 10:32:08 +0100

    Fix name in FormExporter

--- nbgrader/server_extensions/formgrader/exporters/__init__.py
+++ nbgrader/server_extensions/formgrader/exporters/__init__.py
@@ -10,7 +10,7 @@ from bs4 import BeautifulSoup
 # Classes
 #-----------------------------------------------------------------------------
 
-class MyExporter(HTMLExporter):
+class FormExporter(HTMLExporter):
     """
     My custom exporter
     """
